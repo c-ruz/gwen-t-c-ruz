3d3ef88d56df249ec4f62cbf6467ae433849d447
Carlos Ruz <caruz@dcc.uchile.cl>
2023-04-19 23:55:10 -0400

    TEST adds test for RangedCard

---
+++ src/test/scala/UnitCardTests/RangedCardTest.scala
@@ -0,0 +1,23 @@
+package cl.uchile.dcc
+package UnitCardTests
+
+import munit.FunSuite
+
+class RangedCardTest extends FunSuite{
+  val name = "Trotamundos Marah"
+  val str = 7
+  var rangedCard: RangedCard = _
+  override def beforeEach(context: BeforeEach): Unit = {
+    rangedCard = new RangedCard(name, str)
+  }
+  test("A RangedCard can be created with a name and strength") {
+    assertEquals(rangedCard.name, name)
+    assertEquals(rangedCard.str, str)
+  }
+  test("Testing structural equality between RangedCard objects") {
+    assertEquals(new RangedCard(name, str), rangedCard)
+
+    val rangedCard2 = new RangedCard("Trotamundos Nuniq", 4)
+    assert(!rangedCard.equals(rangedCard2))
+  }
+}
