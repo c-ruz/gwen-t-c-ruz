e225777765cfdf2bb3eea154eb20da391ff55ab9
Carlos Ruz <caruz@dcc.uchile.cl>
2023-04-24 10:53:59 -0400

    TEST adds tests for player class: equivalence between players, draw card and hit methods.

--- src/test/scala/gwent/Cards/PlayerTest.scala
+++ src/test/scala/gwent/Cards/PlayerTest.scala
@@ -2,28 +2,27 @@ package cl.uchile.dcc
 package gwent.Cards
 
 import gwent.Cards
-
 import munit.FunSuite
+import scala.collection.mutable.ArrayBuffer
 
 class PlayerTest extends FunSuite {
   val Card1 = new MeleeCard("Eta, el Errabundo", 5)
-  val Card2 = new Card("Zeeh, la primera luz", 3)
-  val Card3 = new Card("Diral, dimensión espiral")
+  val Card2 = new MeleeCard("Zeeh, la primera luz", 3)
+  val Card3 = new SiegeCard("Diral, dimensión espiral", 5)
   val Card4 = new WeatherCard("Escarcha mordiente")
   val Card5 = new WeatherCard("Clima despejado")
+  val deckName = "Xan-kei deck"
   val gems = 3
+  var deckCap = 5
+  var handCap = 3
   var player1: Player = _
   var deck1: Deck = _
   var hand1: Hand = _
-  var deckHolding: Int = _
-  var handHolding: Int = _
 
   override def beforeEach(context: BeforeEach): Unit = {
-    deck1 = new Deck(Set(Card1, Card2, Card3, Card4, Card5), "Xan-kei deck", 5)
+    deck1 = new Deck(ArrayBuffer(Card1, Card2, Card3, Card4, Card5), deckName, deckCap)
     hand1 = new Hand(3)
     player1 = new Player("Player1", gems, deck1, hand1)
-    deckHolding = deck1.holding
-    handHolding = hand1.holding
   }
 
   test("A player can be created with a name, amount of gems, deck and hand assigned") {
@@ -32,23 +31,38 @@ class PlayerTest extends FunSuite {
     assertEquals(player1.deck, deck1)
     assertEquals(player1.hand, hand1)
   }
-  test("Player can draw a set of cards from their deck") {
-    var n = 3
-    assertEquals(player1.hand.holding, 0)
-    player1.draw(n)
-    assertEquals(player1.hand.holding, handHolding+n)
-    assertEquals(player1.deck.holding, deckHolding-handHolding)
 
-    //player cannot exceed hand capacity
-    n = 1
-    assertEquals(player1.hand.holding, player1.hand.capacity)
-    val expected = player1.hand.holding
-    val expected2 = player1.deck.holding
-    player1.draw(n)
-    assertEquals(expected, player1.hand.holding)
-    assertEquals(expected2, player1.deck.holding)
+  test("Two players are the same if they have the same name and deck") {
+    assertEquals(player1, new Player("Player1",gems,deck1,hand1))
+
+    assert(!player1.equals(new Player("Player2",gems,deck1,hand1)))
+
+    val deck2 = new Deck("Empty deck", deckCap)
+    val hand2 = new Hand(handCap)
+    assert(!player1.equals(new Player("Player1", gems, deck2, hand2)))
+
+    assert(!player1.equals(Card1))
+
   }
-  test("Player can play cards from their hand") {
-    //player.play(n) uses hand.getCard(n) method, and sets them in their respective board section
+
+  test("Player can draw n cards from their deck") {
+    // the standard draw amount is 3
+    player1.draw()
+    assertEquals(player1.hand.holding, 3)
+
+    val expectedHand = new Hand(ArrayBuffer(Card1,Card2,Card3), handCap)
+    val expectedDeck = new Deck(ArrayBuffer(Card4,Card5), deckName, deckCap)
+    assertEquals(player1.hand, expectedHand)
+    assertEquals(player1.deck, expectedDeck)
+
+    // player cannot exceed hand capacity or empty the deck
+    player1.draw(1)
+    assertEquals(player1.hand, expectedHand)
+    assertEquals(player1.deck, expectedDeck)
+  }
+
+  test("A player can get hit") {
+    player1.hit()
+    assert(player1.gems == 2)
   }
 }
