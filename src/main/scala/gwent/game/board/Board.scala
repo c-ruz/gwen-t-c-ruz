b23ce5c3b1360663d1ba151918992da933715924
Carlos Ruz <caruz@dcc.uchile.cl>
2023-05-28 17:49:53 -0400

    feat(board): adds equals implementation

--- src/main/scala/gwent/game/board/Board.scala
+++ src/main/scala/gwent/game/board/Board.scala
@@ -3,7 +3,7 @@ package gwent.game.board
 
 import gwent.game.card.{MeleeCard, RangedCard, SiegeCard, WeatherCard}
 
-class Board() {
+class Board() extends Equals {
   /**
    * ================
    * Player board
@@ -24,7 +24,7 @@ class Board() {
    * ==================
    * initially empty, idk if that works
    */
-  private var _weatherSlot: List[WeatherCard] = List() 
+  private var _weatherSlot: List[WeatherCard] = List()
 
   /**
    * ========
@@ -36,27 +36,38 @@ class Board() {
   def WeatherSlot: List[WeatherCard] = _weatherSlot
 
   /**
-   * ========
-   * Setters
-   * ========
-   */
-  
-  
-  /**
    * ===============
    * Class methods
    * ===============
    */
   def placeWeather(card: WeatherCard): Unit = {
-    _weatherSlot = card :: _weatherSlot 
+    _weatherSlot = card :: _weatherSlot
   }
-  
-  
+
+  /**
+   * =====================
+   * Equals Implementation
+   * =====================
+   */
+
+  override def canEqual(that: Any): Boolean = that.isInstanceOf[Board]
+
+  override def equals(obj: Any): Boolean = {
+    if (canEqual(obj)) {
+      val other = obj.asInstanceOf[Board]
+      (this eq other) || (this.playerArmy == other.playerArmy &&
+        this.computerArmy == other.computerArmy && this.WeatherSlot == other.WeatherSlot)
+    }
+    else {
+      false
+    }
+  }
 
 
 
 
 
-  
+
+
 
 }
