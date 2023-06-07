a01e229eb09c8605f9ed2138fe7ade91297e6dc9
Carlos Ruz <caruz@dcc.uchile.cl>
2023-06-07 13:11:08 -0400

    test(controllerTest): adds test for controller transitions through states

---
+++ src/test/scala/gwent/game/states/ControllerTest.scala
@@ -0,0 +1,184 @@
+package cl.uchile.dcc
+package gwent.game.states
+
+import cl.uchile.dcc.gwent.game.Controller
+import munit.FunSuite
+import org.junit.Assert.assertThrows
+
+class ControllerTest extends FunSuite{
+  /**
+   * Testing transitions only
+   */
+  var controller: Controller = _
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    controller = new Controller()
+    // start gaming
+    controller.startGame()
+  }
+
+  test("Game starts in Start state") {
+    assert(controller.isStart)
+  }
+
+  test("Start state can transition to FirstDraw") {
+    // invalid transition
+    assertThrows(classOf[Exception], () => controller.bothDraw())
+    assertThrows(classOf[Exception], () => controller.play())
+    assertThrows(classOf[Exception], () => controller.pass())
+    assertThrows(classOf[Exception], () => controller.hit())
+    assertThrows(classOf[Exception], () => controller.initialDraw())
+    // valid transition
+    controller.initializeGame()
+    assert(controller.isFirstDraw)
+  }
+
+  test("FirstDraw can transition to PlayerTurn") {
+    // set controller
+    controller.initializeGame()
+    // invalid transition
+    assertThrows(classOf[Exception], () => controller.initializeGame())
+    assertThrows(classOf[Exception], () => controller.play())
+    assertThrows(classOf[Exception], () => controller.pass())
+    assertThrows(classOf[Exception], () => controller.hit())
+    assertThrows(classOf[Exception], () => controller.bothDraw())
+    // set controller
+    controller.initialDraw()
+    assert(controller.isPlayerTurn)
+  }
+
+  test("PlayerTurn can transition to CPUTurn") {
+    // set controller
+    controller.initializeGame()
+    controller.initialDraw()
+    // invalid transition
+    assertThrows(classOf[Exception], () => controller.initializeGame())
+    assertThrows(classOf[Exception], () => controller.bothDraw())
+    assertThrows(classOf[Exception], () => controller.hit())
+    assertThrows(classOf[Exception], () => controller.initialDraw())
+    // valid transition
+    controller.play()
+    assert(controller.isCpuTurn)
+  }
+
+  test("PlayerTurn can transition to CpuExtendTurn") {
+    // set controller
+    controller.initializeGame()
+    controller.initialDraw()
+    // valid transition
+    controller.pass()
+    assert(controller.isCpuExtendedTurn)
+  }
+
+  test("CpuTurn can transition to PlayerTurn") {
+    // set controller
+    controller.initializeGame()
+    controller.initialDraw()
+    controller.play()
+    // invalid transition
+    assertThrows(classOf[Exception], () => controller.initializeGame())
+    assertThrows(classOf[Exception], () => controller.bothDraw())
+    assertThrows(classOf[Exception], () => controller.hit())
+    assertThrows(classOf[Exception], () => controller.initialDraw())
+    // valid transition
+    controller.play()
+    assert(controller.isPlayerTurn)
+  }
+
+  test("CpuTurn can transition to PlayerExtendedTurn") {
+    // set controller
+    controller.initializeGame()
+    controller.initialDraw()
+    controller.play()
+    // valid transition
+    controller.pass()
+    assert(controller.isPlayerExtendedTurn)
+  }
+
+  test("CpuExtendedTurn can transition to itself") {
+    // set controller
+    controller.initializeGame()
+    controller.initialDraw()
+    controller.pass()
+    // invalid transition
+    assertThrows(classOf[Exception], () => controller.initializeGame())
+    assertThrows(classOf[Exception], () => controller.bothDraw())
+    assertThrows(classOf[Exception], () => controller.hit())
+    assertThrows(classOf[Exception], () => controller.initialDraw())
+    // valid transition
+    controller.play()
+    assert(controller.isCpuExtendedTurn)
+  }
+
+  test("Cpu extended turn can transition to EndPhase") {
+    // set controller
+    controller.initializeGame()
+    controller.initialDraw()
+    controller.pass()
+    // valid transition
+    controller.pass()
+    assert(controller.isEndPhase)
+  }
+
+  test("PlayerExtendedTurn can transition to itself") {
+    // set controller
+    controller.initializeGame()
+    controller.initialDraw()
+    controller.play()
+    controller.pass()
+    // invalid transition
+    assertThrows(classOf[Exception], () => controller.initializeGame())
+    assertThrows(classOf[Exception], () => controller.bothDraw())
+    assertThrows(classOf[Exception], () => controller.hit())
+    assertThrows(classOf[Exception], () => controller.initialDraw())
+    // valid transition
+    controller.play()
+    assert(controller.isPlayerExtendedTurn)
+  }
+
+  test("PlayerExtendedTurn can transition to EndPhase") {
+    // set controller
+    controller.initializeGame()
+    controller.initialDraw()
+    controller.play()
+    controller.pass()
+    // valid transition
+    controller.pass()
+    assert(controller.isEndPhase)
+  }
+
+  test("EndPhase can return to RoundStart") {
+    // set controller
+    controller.initializeGame()
+    controller.initialDraw()
+    controller.pass()
+    controller.pass()
+    // invalid transition
+    assertThrows(classOf[Exception], () => controller.initializeGame())
+    assertThrows(classOf[Exception], () => controller.play())
+    assertThrows(classOf[Exception], () => controller.pass())
+    assertThrows(classOf[Exception], () => controller.bothDraw())
+    assertThrows(classOf[Exception], () => controller.initialDraw())
+    // valid transition
+    controller.hit()
+    assert(controller.isRoundStart)
+  }
+
+  test("RoundStart can transition to PlayerTurn") {
+    // set controller
+    controller.initializeGame()
+    controller.initialDraw()
+    controller.pass()
+    controller.pass()
+    controller.hit()
+    // invalid transition
+    assertThrows(classOf[Exception], () => controller.initializeGame())
+    assertThrows(classOf[Exception], () => controller.play())
+    assertThrows(classOf[Exception], () => controller.pass())
+    assertThrows(classOf[Exception], () => controller.hit())
+    assertThrows(classOf[Exception], () => controller.initialDraw())
+    // valid transition
+    controller.bothDraw()
+    assert(controller.isPlayerTurn)
+  }
+}
