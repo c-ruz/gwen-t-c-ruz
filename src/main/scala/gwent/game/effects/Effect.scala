52a97e73674002d1d58b8d5d0854edb880c0587a
Carlos Ruz <caruz@dcc.uchile.cl>
2023-07-15 14:40:34 -0400

    FEAT(Effects): Added equals implementation for effects. In short, all effects of the same type are equal.

--- src/main/scala/gwent/game/effects/Effect.scala
+++ src/main/scala/gwent/game/effects/Effect.scala
@@ -6,12 +6,31 @@ import gwent.game.card.Card
 import cl.uchile.dcc.gwent.game.board.Board
 import cl.uchile.dcc.gwent.game.card.units.UnitCard
 
-trait Effect {
-  
+trait Effect extends Equals {
+  /**
+   * Effect name getter.
+   * @return A string with the name of the effect.
+   */
   def name: String
-  
+
+  /**
+   * Effect description getter
+   * @return A string with the description of the effect.
+   */
   def description: String
+
+  /**
+   * Apply the effect of the card.
+   * @param self  The card with the effect.
+   * @param target  Target row of cards.
+   */
+  def apply(self: Card, target: List[UnitCard]): Unit
   
-  def activate(row: List[UnitCard]): Unit
-  
+  override def equals(obj: Any): Boolean = {
+    if (canEqual(obj)) {
+      true
+    } else {
+      false
+    }
+  }
 }
