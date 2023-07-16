ac3baf319d170baa40300503991377be8e847a79
Carlos Ruz <caruz@dcc.uchile.cl>
2023-07-16 17:57:32 -0400

    REFACTOR(controller): StartGame() now is a transition for states and also adds Player and register itself as Observer for them

--- src/main/scala/gwent/game/Controller.scala
+++ src/main/scala/gwent/game/Controller.scala
@@ -1,25 +1,56 @@
 package cl.uchile.dcc
 package gwent.game
 
-import gwent.game.states._
+import gwent.game.states.*
 
-class Controller {
-  var state: State = _
+import cl.uchile.dcc.gwent.game.board.Board
+import cl.uchile.dcc.gwent.game.factory.deck.{DeckFactory, FlyingGangDeckFactory, XanKeiDeckFactory}
+import cl.uchile.dcc.gwent.game.factory.players.{CpuFactory, HumanFactory}
+import cl.uchile.dcc.gwent.game.players.{Computer, ISubject, Player}
+
+import scala.collection.mutable.ListBuffer
+
+class Controller(deckFactory1: DeckFactory, deckFactory2: DeckFactory) extends Observer {
+
+  val playerCapsule: ListBuffer[Player] = ListBuffer.empty
+  val cpuCapsule: ListBuffer[Computer] = ListBuffer.empty
+  private val player1Factory: HumanFactory = new HumanFactory
+  private val player2Factory: CpuFactory = new CpuFactory
+
+  var state: State = new StartState
+  var board: Board = new Board()
+  private var gameActive: Boolean = false
+  private var endMessage: String = _
+
+  def getGameState: Boolean = gameActive
+  def getEndMessage: String = endMessage
   
   def setState(aState: State) = {
     state = aState
   }
   
   def startGame(): Unit = {
-    state = new StartState()
+    gameActive = true
+
+    player1Factory.setDeckFactory(deckFactory1)
+    player2Factory.setDeckFactory(deckFactory2)
+    val player1 = player1Factory.create(board)
+    val player2 = player2Factory.create(board)
+
+    player1.registerObserver(this)
+    player2.registerObserver(this)
+    playerCapsule += player1
+    cpuCapsule += player2
+
+    state.startGame(this)
   }
 
   def bothDraw(): Unit = state.bothDraw(this)
-  def play(): Unit = state.play(this)
+  def play(index: Int): Unit = state.play(this, index)
   def pass(): Unit = state.pass(this)
   def hit(): Unit = state.hit(this)
-  def initializeGame(): Unit = state.initializeGame(this)
   def initialDraw(): Unit = state.initialDraw(this)
+  def reset(): Unit = state.reset(this)
 
   def isStart: Boolean = state.isStart
   def isFirstDraw: Boolean = state.isFirstDraw
@@ -29,4 +60,45 @@ class Controller {
   def isCpuExtendedTurn: Boolean = state.isCpuExtendedTurn
   def isEndPhase: Boolean = state.isEndPhase
   def isRoundStart: Boolean = state.isRoundStart
+  def isGameFinished: Boolean = state.isGameFinished
+
+  override def updatePlayer(s: ISubject, arg: Any): Unit = {
+    val remaining = arg.asInstanceOf[Int]
+    if (remaining <= 0){
+      endMessage = "The winner is Player 2"
+      gameActive = false
+    }
+    else {
+      if (remaining > 1) {
+        println(s"Player 1 has $remaining gems remaining.")
+      }
+      else {
+        println(s"Player 1 has $remaining gem remaining.")
+      }
+    }
+  }
+
+  override def updateCpu(s: ISubject, arg: Any): Unit = {
+    val remaining = arg.asInstanceOf[Int]
+    if (remaining <= 0) {
+      endMessage = "The winner is Player 1"
+      gameActive = false
+    }
+    else {
+      if (remaining > 1) {
+        println(s"Player 2 has $remaining gems remaining.")
+      }
+      else {
+        println(s"Player 2 has $remaining gem remaining.")
+      }
+    }
+  }
+
+  def drawHandler(): Unit = {
+    if (playerCapsule.head.gems == 0 &&
+    cpuCapsule.head.gems == 0){
+      endMessage = "It's a Draw"
+      gameActive = false
+    }
+  }
 }
