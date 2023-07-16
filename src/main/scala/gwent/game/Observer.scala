5333b01edad0bd9436e586cb0971d608c6fb2756
Carlos Ruz <caruz@dcc.uchile.cl>
2023-07-16 14:51:40 -0400

    REFACTOR(Observer): Divided update method into updatePlayer and updateCpu, for better management of notifications fro player1 and player2 respectively

--- src/main/scala/gwent/game/Observer.scala
+++ src/main/scala/gwent/game/Observer.scala
@@ -4,5 +4,6 @@ package gwent.game
 import gwent.game.players.ISubject
 
 trait Observer {
-  def update(s: ISubject, arg: Any): Unit
+  def updatePlayer(s: ISubject, arg: Any): Unit
+  def updateCpu(s: ISubject, arg: Any): Unit
 }
